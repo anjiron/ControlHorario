ce3439c63c9d3248c48dd890d90e84b9d17246b0
anjiron <75119091+anjiron@users.noreply.github.com>
2021-01-31 10:51:56 +0100

    Preparación_setup

--- setup.py
+++ setup.py
@@ -1,24 +1,47 @@
+
 import os
+import sys
+import shutil
+
 import db_control as db
 
 
 class setup():
     def __init__(self):
+        abs_path = os.path.split(os.path.abspath(__file__))
+        path = abs_path[0]
+
         self.install_pandas()
-        # self.intall_tkinter()
-        # self.crear_db_and_pickle()
+        self.install_pyinstaller()
+        self.crear_db_and_pickle(path)
+        self.crear_exe_main(path)
 
     def install_pandas(self):
         os.system('pip install pandas')
 
+    def install_pyinstaller(self):
+        os.system('pip install pyinstaller')
 
-    def intall_tkinter(self):
-        os.system('pip install python-tk')
-
-
-    def crear_db_and_pickle(self):
+    def crear_db_and_pickle(self, path):
         db.new_db("control_horario")
-        db.save_pickle({'fecha': 0, 'hora_entrada': 0}, 'C:/Users/imdt/Documents/control_horario', 'temporal_data')
+        db.save_pickle({'fecha': 0, 'hora_entrada': 0}, path, 'temporal_data')
+
+    def move_files(self, file_name, new_path, old_path):
+        sistema = sys.platform
+        print('fasjdfñlaksdjfñlaksdjfasldkfjñlasdfkja', sistema)
+        if sistema == 'linux':
+            os.system('sudo cp -R ' + str(old_path) + str(file_name) + " " + str(new_path))
+        elif sistema == 'win32':
+            os.system('MOVE ' + str(old_path) + str(file_name) + " " + str(new_path))
+
+
+    def crear_exe_main(self, path):
+        old_path = str(path) + '\dist'
+        os.system('pyinstaller --windowed --onefile main.py')
+        self.move_files('\main.exe', path, old_path)
+        shutil.rmtree(old_path)
+        shutil.rmtree(str(path) + '/build')
+        os.remove(str(path) + '/main.spec')
 
 
 if __name__ == '__main__':
